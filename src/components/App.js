60e7bc287b865c28cd6e834e6ae3dcd7334cc701
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 19:35:10 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -64,7 +64,7 @@ export default function App() {
     // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
     //    helper to [POST] `newFriend` to `http://localhost:4000/friends`
     //    and regardless of success or failure, the form should reset
-    axios.post("http://localhost:4000/friends", formValues)
+    axios.post("http://localhost:4000/friends", newFriend)
       .then(res => {
         setFriends([...friends, res.data])
       })
