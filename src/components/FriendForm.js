4b9dd679a2bffcc7b20f3a859309076b06ce7ec0
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 18:58:02 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -16,8 +16,8 @@ export default function FriendForm(props) {
 
   const onChange = evt => {
     /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-    const { name, value } = evt.target
-    change(name, value)
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? 
   }
 
   return (
