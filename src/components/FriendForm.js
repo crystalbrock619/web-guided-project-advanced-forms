3bb188c1dde710d76b20806e0ea57564d3faeb6e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 18:45:22 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -87,7 +87,7 @@ export default function FriendForm(props) {
             name="civil"
             value={values.civil}
             checked={values.civil === 'single'}
-            onChange={}
+            onChange={onChange}
           />
         </label>
 
