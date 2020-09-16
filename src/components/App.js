e97d23fc8aeb53237a0864ea437637c786137a5d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 19:30:47 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -50,12 +50,13 @@ export default function App() {
   const getFriends = () => {
     // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
     //    helper to [GET] all friends from `http://localhost:4000/friends`
-    axios.get('http://localhost:4000/friends')
+    axios.get(`http://localhost:4000/friends`)
       .then(res => {
-
+        setFriends(res.data)
       })
       .catch(err => {
-        
+        debugger
+        console.log(err)
       })
   }
 
