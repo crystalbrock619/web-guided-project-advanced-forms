d235f8038351a703ab4f46bc2bcb90b8234e176a
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 19:29:57 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -50,6 +50,13 @@ export default function App() {
   const getFriends = () => {
     // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
     //    helper to [GET] all friends from `http://localhost:4000/friends`
+    axios.get('http://localhost:4000/friends')
+      .then(res => {
+
+      })
+      .catch(err => {
+        
+      })
   }
 
   const postNewFriend = newFriend => {
