d69b4781e035e67c0b993e1274080951fd9a2fea
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 20:02:37 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -80,6 +80,27 @@ export default function App() {
 
   const validate = (name, value) => {
     // let's validate this specific key/value
+        // yup.reach will allow us to "reach" into the schema and test only one part.
+    // We give reach the schema as the first argument, and the key we want to test as the second.
+    yup
+      .reach(formSchema, e.target.name)
+      //we can then run validate using the value
+      .validate(e.target.value)
+      // if the validation is successful, we can clear the error message
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      /* if the validation is unsuccessful, we can set the error message to the message 
+        returned from yup (that we created in our schema) */
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
   }
 
   //////////////// EVENT HANDLERS ////////////////
