38e3b1935e39b696513f21ac5255658176522344
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 18:47:46 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -92,7 +92,13 @@ export default function FriendForm(props) {
         </label>
 
         <label>Married
-
+          <input
+            type='radio'
+            name='civil'
+            value='married'
+            checked={values.civil === 'married'}
+            onChange={onChange}
+          />
         </label>
       </div>
 
