5519087be37e90f28ae965504757912244b3dead
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-16 19:23:44 +0200

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -16,19 +16,3 @@ export default yup.object().shape({
   reading: yup.boolean(),
   coding: yup.boolean(),
 })
-
-// Here goes the schema for the form
-// const formSchema = Yup.object().shape({
-//   email: Yup
-//     .string()
-//     .email("Must be a valid email address.")
-//     .required("Must include email address."),
-//   password: Yup
-//     .string()
-//     .min(6, "Passwords must be at least 6 characters long.")
-//     .required("Password is Required")
-//   terms: Yup
-//     .boolean()
-//     .oneOf([true], "You must accept Terms and Conditions")
-//     // required isn't required for checkboxes.
-// });
